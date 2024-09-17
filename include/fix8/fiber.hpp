571731fe59cc800ddaf5a55f9373968331b9bf7b
David Dight <dakka@fix8.org>
2024-09-17 17:08:55 +1000

    pre-release-2.4a

--- include/fix8/fiber.hpp
+++ include/fix8/fiber.hpp
@@ -513,7 +513,7 @@ public:
 #if defined _MSC_VER
 __asm{R"(.text
 .align 16
-.type coroswitch,@function
+.global _asm_calculate_reals
 coroswitch:
 	cmpq %rdx,%rcx			/* prevent self-switch */
 	jne _doswitch
