56d2731af97f5a01dcef80c01902c43ddbb06112
David Dight <dakka@fix8.org>
2024-09-17 15:03:16 +1000

    pre-release-2.4a

--- include/fix8/fiber.hpp
+++ include/fix8/fiber.hpp
@@ -430,8 +430,8 @@ class alignas(64) fiber_base
 #if defined _MSC_VER
 		std::memset(_stk - 26, 0x0, 26 * sizeof(uintptr_t)); // zero: rsi,rdi,rbp,r12,r13,r14,r15,xmm6-xmm15
 		_stk -= 27; // include flags
-		asm(stmxcsr (*reinterpret_cast<uint32_t*>(_stk))); // preserve lower dword
-		asm(fnstcw (*(reinterpret_cast<uint32_t*>(_stk) + 1))); // preserve upper dword, lower word
+		__asm(stmxcsr (*reinterpret_cast<uint32_t*>(_stk))); // preserve lower dword
+		__asm(fnstcw (*(reinterpret_cast<uint32_t*>(_stk) + 1))); // preserve upper dword, lower word
 #else
 		std::memset(_stk - 6, 0x0, 6 * sizeof(uintptr_t)); // zero: rdi,rbp,r12,r13,r14,r15
 		_stk -= 7; // include flags
@@ -502,7 +502,7 @@ public:
 // static void fiber_base::coroswitch(fiber_base *old, fiber_base *newer) noexcept; //aka _coroswitch
 // TODO other ABI
 #if defined _MSC_VER
-asm(R"(.text
+__asm(R"(.text
 .align 16
 .type coroswitch,@function
 coroswitch:
