9b9b462069aef64987f60f7d4bdf1e4e8c04ccb8
David Dight <dakka@fix8.org>
2024-09-24 13:53:11 +1000

    pre-release-2.4a

--- include/fix8/fiber.hpp
+++ include/fix8/fiber.hpp
@@ -511,7 +511,7 @@ __declspec(allocate(".text")) static constexpr unsigned char coroswitch_code[]
 		0x41, 0xff, 0xe0
 	};
 	using call_func = void (*)(fiber_base *old, fiber_base *newer);
-__declspec(allocate(".text")) static inline call_func coroswitch { reinterpret_cast<call_func>(static_cast<const unsigned char *>(coroswitch_code)) };
+	static inline call_func coroswitch { reinterpret_cast<call_func>(static_cast<const unsigned char *>(coroswitch_code)) };
 
 	__declspec(noinline) static size_t get_default_stacksz()
 	{
